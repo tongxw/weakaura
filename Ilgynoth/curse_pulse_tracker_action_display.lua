0f5d100f0ee53c037dfec05239e702142e4124ce
Will(Xiaowei) Tong <willtxw@gmail.com>
2020-07-07 16:49:53 -0700

    ilgynoth string

---
+++ Ilgynoth/curse_pulse_tracker_action_display.lua
@@ -0,0 +1,3 @@
+if aura_env.state and aura_env.state.duration then
+    aura_env.show()
+end
---
+++ Ilgynoth/curse_pulse_tracker_trigger.lua
@@ -0,0 +1,88 @@
+function(states, event, ...)
+    if event == "OPTIONS" then
+        local sound = aura_env.config.sound
+        states[""] = {
+            name = "Cursed Pulse",
+            icon = 1392546,
+            amount = AbbreviateNumbers(123456),
+            additionalProgress = {{min=0, max=-0.5}},
+            sound = sound,
+            progressType = "timed",
+            duration = 19.2,
+            expirationTime = GetTime() + 19.2,
+            autoHide = true,
+            changed = true,
+            show = true
+        }
+        aura_env.show()
+        return true
+    end
+    
+    if event == "COMBAT_LOG_EVENT_UNFILTERED" and ... then
+        local _, message, _, srcGUID, srcName, _, _, destGUID, destName, _, destRaidFlags, spellid, _, _, arg1, _, arg2 = ...
+        local t = WeakAuras.triggerState[aura_env.id].triggers
+        
+        if t[2] then
+            if message == "SPELL_DAMAGE" and spellid == 316813 and destGUID == WeakAuras.myGUID then
+                --raw spell damage taken
+                if not states[""] or states[""].expirationTime - GetTime() <= 0.2 then
+                    local sound = aura_env.config.sound
+                    states[""] = {
+                        name = "Cursed Pulse",
+                        icon = 1392546,
+                        amount = AbbreviateNumbers(arg1),
+                        additionalProgress = {{min=0, max=-0.5}},
+                        sound = sound,
+                        progressType = "timed",
+                        duration = 19.2,
+                        expirationTime = GetTime() + 19.2,
+                        autoHide = true,
+                        changed = true,
+                        show = true
+                    }
+                end
+            elseif message == "SPELL_MISSED" and spellid == 316813 and destGUID == WeakAuras.myGUID and arg1 == "ABSORB" then
+                --full spell damage absorbed
+                if not states[""] or states[""].expirationTime - GetTime() <= 0.2 then
+                    local sound = aura_env.config.sound
+                    states[""] = {
+                        name = "Cursed Pulse",
+                        icon = 1392546,
+                        amount = AbbreviateNumbers(arg2),
+                        additionalProgress = {{min=0, max=-0.5}},
+                        sound = sound,
+                        progressType = "timed",
+                        duration = 19.2,
+                        expirationTime = GetTime() + 19.2,
+                        autoHide = true,
+                        changed = true,
+                        show = true
+                    }
+                end
+            end
+        end
+        
+        if message == "SPELL_AURA_REMOVED" and spellid == 313759 and destGUID == WeakAuras.myGUID and states[""] then
+            states[""].show = false
+            states[""].changed = true
+        end
+        
+    end
+    
+    if event == "WA_CANCEL_PULSE" and ... and states[""] then
+        states[""].show = false
+        states[""].changed = true
+    end
+    
+    return true
+end
+
+
+{
+    expirationTime = true,
+    sound = "bool",
+    additionalProgress = {
+        [1] = {}
+    },
+}
+
